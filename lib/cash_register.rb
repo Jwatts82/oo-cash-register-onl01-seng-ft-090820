159edc2b69a554244a0b44cd79b294760448f5bd
Jessica Watts <jessperr182@hotmail.com>
2020-09-20 23:27:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,10 +8,18 @@ class CashRegister
   end
   
   def add_item(item, price, quantity = 1)
-    @item = item
     @price = price
-    @quantity
-
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
 
 
 end
